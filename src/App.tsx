111a39ac04c27b1ed1011c8b1275691641a85c77
Piero Borrelli <borrellipiero99@gmail.com>
2023-06-19 18:46:21 +0200

    Add global css reset style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import GlobalStyles from "./GlobalStyles";
 function App() {
-  return <span>okok</span>;
+  return (
+    <>
+      <span>okok</span>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
